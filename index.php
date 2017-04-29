a361222572b13f1cce03a7bc891b9a94bd3e01bb
PavlikSedoy <pavliksedoy@gmail.com>
2017-04-29 09:50:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sedoy
+ * Date: 12.03.17
+ * Time: 14:21
+ */
+
+// FRONT CONTROLLER
+
+// 1. Общие настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+session_start();
+
+// 2. Подключение файлов системы
+define('ROOT', dirname(__FILE__));
+require_once (ROOT. '/components/Autoload.php');
+
+// 3. Вызов Router
+$router = new Router();
+$router->run();
